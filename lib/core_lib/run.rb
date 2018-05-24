34fcc679235001d0bba26167fdce39f30db5b1d4
inoicouldalwaysturn2u <iknowicouldalwaysturn@gmail.com>
2018-05-24 18:32:52 -0400

    Bunch of mostly smaller things
    - Changed the Sheetsu variable from client to sheetsu since now there’s a Pushover client variable too (named pushover).
    - Moved most of the code from the main go/sheet_shiz methods. Put them into their own 3 methods, though one is with new code
    - total adjusted amounts in its own method and using its own Hashie::Mash
    - sheet method only contains sheet row creation now
    - new formulas method because Sheetsu never got back to me about the issue with arrayformulas in sheets (they seemed to go away when Sheetsu added a new row of content). So doing formulas here.
    - Moved around a bit of code for organizational reasons
    - Yes, the code isn’t the prettiest or well organized. This class is far too big and unwieldly now

--- lib/core_lib/run.rb
+++ lib/core_lib/run.rb
@@ -2,9 +2,27 @@ class Run
   attr_reader :headless, :wrap, :browser
   attr_reader :base_logger
   
+  
+  ## Initial
+  
   def initialize
     @base_logger = Rails.logger.loop
   end
+  
+  def sheetsu
+    url = ENV[ 'sheetsu_api_url_id' ]
+    @sheetsu ||= Sheetsu::Client.new( url )
+  end
+  
+  def pushover
+    Pushover.configure do | config |
+      config.user = ENV[ 'pushover_user' ]
+      config.token = ENV[ 'pushover_token' ]
+    end
+  end
+  
+  
+  ## Core
   
   def go( display_number = nil )
     @base_logger.info 'BEGIN - Run - go'
@@ -18,8 +36,13 @@ class Run
         login_result = @login.login
         
         @container = @pa.run
+        total_adjusted = total_adjusted_amounts( @container )
+    
+        @formulas = formulas_replacement( total_adjusted, @container )
         
-        result = sheet_shiz( @container )
+        result = sheet_shiz( total_adjusted, @container, @formulas )
+        
+        
       ensure
         @wrap.browser.close if @wrap.browser.is_open?
       end
@@ -29,35 +52,18 @@ class Run
     @base_logger.info 'END - Run - go'
     result
   end
-  
-  def client
-    url = ENV[ 'sheetsu_api_url_id' ]
-    @client ||= Sheetsu::Client.new( url )
-  end
-  
-  def cleanse_tile_text( text )
-    cleansed_text = text.sub( '$', '' )
-    cleansed_text.sub( ',', '' )
-  end
-  
-  def sheet_shiz( container, sheet_client = nil )
-    sheet_client = client if sheet_client.nil?
     
-    # well this isn't pretty
-    total_revenue = container.total_revenue
-    total_orders = container.number_of_orders
-    total_fulfillment = container.total_fulfillment_costs
-    if !adjustment_last_row.nil?
-      total_revenue = adjusted_revenue( cleanse_tile_text( adjustment_last_row[ 'Total Revenue' ] ).to_i, container.total_revenue )
-      total_orders = adjusted_number_of_orders( cleanse_tile_text( adjustment_last_row[ 'Total Shopify Orders' ] ).to_i, container.number_of_orders )
-      total_fulfillment = adjusted_fulfillment( cleanse_tile_text( adjustment_last_row[ 'Fulfillment' ] ).to_i, container.total_fulfillment_costs )
+    # copy pasted, needed here
+    def cleanse_tile_text( text )
+      cleansed_text = text.sub( '$', '' )
+      cleansed_text.sub( ',', '' )
     end
-    
-    
-    ## Formulas [for now]
-    profit = total_revenue - ( total_fulfillment_costs + container.ad_spend + container.transaction_fees )
-    roi = profit / container.ad_spend * 100
-    conversion_rate = total_orders / container.unique_visitors * 100
+  
+  
+  ## Core integrations
+  
+  def sheet_shiz( total_adjusted, container, formulas, sheet_client = nil )
+    sheet_client = sheetsu if sheet_client.nil?
     
     sheet_client.create(
       {
@@ -67,21 +73,144 @@ class Run
         :'Day of Week' => day_of_week,
         Time: row_time,
 
-        :'Total Revenue' => total_revenue,
-        :'Total Shopify Orders' => total_orders,
+        :'Total Revenue' => total_adjusted.revenue,
+        :'Total Shopify Orders' => total_adjusted.orders,
         :'Spend Fomo1' => container.ad_account_1,
         :'Spend Fomo2' => container.ad_account_2,
         :'Spend Fomo4' => container.ad_account_4,
         Spend: container.ad_spend,
-        Fulfillment: total_fulfillment,
+        Fulfillment: total_adjusted.fulfillment,
         Fees: container.transaction_fees,
         :'Refunds in Shopify' => container.refunds,
         :'Unique Visitors' => container.unique_visitors,
-        :'The Profit' => profit,
-        ROI: roi,
-        :'Conversion Rate' => conversion_rate
+        :'The Profit' => @formulas.profit,
+        ROI: @formulas.roi,
+        :'Conversion Rate' => @formulas.conversion_rate,
+        
+        :'Last Check Spend' => @formulas.last_check.spend,
+        :'Last Check Profit' => @formulas.last_check.profit,
+        :'Last Check ROI' => @formulas.last_check.roi,
+        :'Last Check Conversion' => @formulas.last_check.conversion_rate,
+        
+        :'2nd Last Check Spend' => @formulas.second_last_check.spend,
+        :'2nd Last Check Profit' => @formulas.second_last_check.profit,
+        :'2nd Last Check ROI' => @formulas.second_last_check.roi,
+        :'2nd Last Check Conversion' => @formulas.second_last_check.conversion_rate,
       }
     )
+  end
+  
+  def pushover_notification( total_adjusted, container, formulas, pushover_client = nil )
+    
+  end
+  
+  
+  ## Segment out shiz
+  
+  def total_adjusted_amounts( container = nil, adjusted_last_row = nil )
+    container = @container if container.nil?
+    adjusted_last_row = adjustment_last_row if adjusted_last_row.nil?
+    
+    # well this isn't pretty
+    adjusted_totals = Hashie::Mash.new
+    adjusted_totals.revenue = container.total_revenue
+    adjusted_totals.orders = container.number_of_orders
+    adjusted_totals.fulfillment = container.total_fulfillment_costs
+    
+    if !adjustment_last_row.nil?
+      adjusted_totals.revenue = adjusted_revenue( cleanse_tile_text( adjusted_last_row[ 'Total Revenue' ] ).to_i, container.total_revenue )
+      adjusted_totals.orders = adjusted_number_of_orders( cleanse_tile_text( adjusted_last_row[ 'Total Shopify Orders' ] ).to_i, container.number_of_orders )
+      adjusted_totals.fulfillment = adjusted_fulfillment( cleanse_tile_text( adjusted_last_row[ 'Fulfillment' ] ).to_i, container.total_fulfillment_costs )
+    end
+    
+    adjusted_totals
+  end
+  
+  def formulas_container
+    @formulas = Hashie::Mash.new
+    
+    @formulas.profit = nil
+    @formulas.roi = nil
+    @formulas.conversion_rate = nil
+    
+    @formulas.last_check = Hashie::Mash.new
+    @formulas.last_check.spend = nil
+    @formulas.last_check.profit = nil
+    @formulas.last_check.roi = nil
+    @formulas.last_check.conversion_rate = nil
+    
+    @formulas.second_last_check = Hashie::Mash.new
+    @formulas.second_last_check.spend = nil
+    @formulas.second_last_check.profit = nil
+    @formulas.second_last_check.roi = nil
+    @formulas.second_last_check.conversion_rate = nil
+    
+    @formulas
+  end
+  
+  # formulas [for now]
+  def formulas_replacement( total_adjusted, container = nil )
+    container = @container if container.nil?
+    
+    @formulas = formulas_container
+    
+    @formulas.profit = total_adjusted.revenue - ( total_adjusted.fulfillment + container.ad_spend + container.transaction_fees )
+    @formulas.roi = @formulas.profit / container.ad_spend * 100
+    @formulas.conversion_rate = total_adjusted.orders.to_f / container.unique_visitors.to_f * 100
+    
+    if !previous_row.nil?
+      @formulas.last_check.spend = container.ad_spend - previous_row[ 'Spend' ]
+      @formulas.last_check.profit = @formulas.profit - previous_row[ 'The Profit' ]
+      @formulas.last_check.roi = @formulas.last_check.profit / @formulas.last_check.spend * 100
+      @formulas.last_check.conversion_rate = ( total_adjusted.orders.to_f - previous_row[ 'Total Shopify Orders' ].to_f ) / ( container.unique_visitors.to_f - previous_row[ 'Unique Visitors' ].to_f ) * 100
+    end
+    
+    if !second_previous_row.nil?
+      @formulas.second_last_check.spend = container.ad_spend - second_previous_row[ 'Spend' ]
+      @formulas.second_last_check.profit = @formulas.profit - second_previous_row[ 'The Profit' ]
+      @formulas.second_last_check.roi = @formulas.second_last_check.profit / @formulas.second_last_check.spend * 100
+      @formulas.second_last_check.conversion_rate = ( total_adjusted.orders.to_f - second_previous_row[ 'Total Shopify Orders' ].to_f ) / ( container.unique_visitors.to_f - second_previous_row[ 'Unique Visitors' ].to_f ) * 100
+    end
+    
+    @formulas
+  end
+  
+  def previous_row_method( sheet_client = nil )
+    sheet_client = sheetsu if sheet_client.nil?
+    
+    begin
+      rows = sheet_client.read(
+        search: { Date: date }
+      )
+    rescue Sheetsu::NotFoundError
+      return nil
+    end
+    
+    rows.last
+  end
+  
+  def previous_row( sheet_client = nil )
+    @previous_row ||= previous_row_method( sheet_client )
+  end
+  
+  def second_previous_row_method( sheet_client = nil )
+    sheet_client = sheetsu if sheet_client.nil?
+    
+    begin
+      rows = sheet_client.read(
+        search: { Date: date }
+      )
+    rescue Sheetsu::NotFoundError
+      return nil
+    end
+    
+    if rows.length > 1
+      return rows[ -2 ]
+    end
+  end
+  
+  def second_previous_row( sheet_client = nil )
+    @second_previous_row ||= second_previous_row_method( sheet_client )
   end
   
   
@@ -122,27 +251,8 @@ class Run
     ENV[ 'adjustment_sheet' ]
   end
   
-  def previous_row_method( sheet_client = nil )
-    sheet_client = client if sheet_client.nil?
-    
-    begin
-      rows = sheet_client.read(
-        search: { Date: date }
-      )
-    rescue Sheetsu::NotFoundError
-      return nil
-    end
-    
-    rows.last
-  end
-  
-  def second_previous_row_method
-    
-  end
-  
-  
   def adjustment_last_row_method( sheet_client = nil, the_adjustment_sheet_name = nil )
-    sheet_client = client if sheet_client.nil?
+    sheet_client = sheetsu if sheet_client.nil?
     the_adjustment_sheet_name = adjustment_sheet_name if the_adjustment_sheet_name.nil?
     
     begin
